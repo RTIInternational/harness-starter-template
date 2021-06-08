afd7017465b01b0aa1b0f0bf8b62edf7abb6d827
Michael Long <michaellong@rti.org>
2021-06-08 10:26:01 -0400

    Update script's git clone repo link

--- dev-setup.sh
+++ dev-setup.sh
@@ -3,13 +3,13 @@ mkdir dev-packages
 cd dev-packages
 
 # install datavue
-git clone git@gitlab.rtp.rti.org:datavue/datavue.git
+git clone git@cds-mallard.rtp.rti.org:datavue/datavue.git
 cd datavue
 yarn install
 cd ../
 
 # install datavue-ui
-git clone git@gitlab.rtp.rti.org:datavue/datavue-ui.git
+git clone git@cds-mallard.rtp.rti.org:datavue/datavue-ui.git
 cd datavue-ui
 yarn install
 cd ../
