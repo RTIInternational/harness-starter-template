7a524f0c8ad24ea13ebbd7ea41e35126ea5eafe1
Michael Long <michaellong@rti.org>
2021-11-05 12:28:14 -0400

    Rename and update harness page file

--- src/harness-pages/tisTheSeason.js
+++ src/harness-pages/tisTheSeason.js
@@ -1,13 +1,35 @@
-import exampleChart from '../components/exampleChart'
-import examplePage from '../components/examplePage'
+import BarChart from '../components/barChart'
+import tisTheSeason from '../components/tisTheSeason'
 import { components } from '@rtidatascience/harness-ui'
-export default class ExamplePage {
-  title = 'Example Page'
-  key = 'examplePage'
-  pageComponent = examplePage
+export default class TisTheSeason {
+  title = 'Tis The Season'
+  key = 'tisTheSeason'
+  pageComponent = tisTheSeason
   pageProps = {}
   retrieveData = async (state, pageObject, hs) => {
-    return { exampleChart: [{ key: 'value', key2: 'value2' }] }
+    return {
+      halloweenMovies:
+      {
+        title: 'Halloween Movies',
+        data:
+        [
+          ['What We Do in the Shadows', 96],
+          ['Halloween', 96],
+          ['The Nightmare Before Christmas', 95],
+          ['It\'s the Great Pumpkin, Charlie Brown', 95],
+          ['The Witches', 93],
+          ['It (2017)', 86],
+          ['The Blair Witch Project', 86],
+          ['The Exorcist', 83],
+          ['Halloweentown', 80],
+          ['Scream', 79]
+        ]
+      },
+      thanksgivingFood:
+      [
+        {}
+      ]
+    }
   }
   filters = function () {
     return {
@@ -85,25 +107,25 @@ export default class ExamplePage {
 
   charts = function () {
     return {
-      exampleChart: {
-        title: 'Example Chart',
+      halloweenMovies: {
+        title: 'Halloween Movies',
         component: components['ChartWithTable'],
         props: {
-          chartComponent: exampleChart,
-          tableAdapter: function (chart, filters, data) {
-            let map = {
-              'key': 'Transformed key',
-              'key2': 'Transformed key 2'
-            }
-            let newData = []
-            data.forEach(datum => {
-              newData.push(Object.keys(datum).reduce((acc, key) => {
-                acc[map[key]] = datum[key].toUpperCase() + ' TRANSFORMED UPPERCASE'
-                return acc
-              }, {}))
-            })
-            return newData
-          }
+          chartComponent: BarChart
+          // tableAdapter: function (chart, filters, data) {
+          //   let map = {
+          //     'key': 'Transformed key',
+          //     'key2': 'Transformed key 2'
+          //   }
+          //   let newData = []
+          //   data.forEach(datum => {
+          //     newData.push(Object.keys(datum).reduce((acc, key) => {
+          //       acc[map[key]] = datum[key].toUpperCase() + ' TRANSFORMED UPPERCASE'
+          //       return acc
+          //     }, {}))
+          //   })
+          //   return newData
+          // }
         }
       }
     }
